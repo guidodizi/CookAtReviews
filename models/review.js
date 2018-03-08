771d439e3f10dd5df317a903a3864fd6b829838c
Guido Dizioli <guidodizi@gmail.com>
2018-03-08 13:32:30 -0300

    image not required

--- models/review.js
+++ models/review.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var ReviewSchema = new Schema({
     name: {type: String, required: true, min: 3, max: 100},
     chef: { type: Schema.ObjectId, ref: 'Chef', required: true },
-    image_url: {type: String, required: true, min: 3, max: 1000},
+    image_url: {type: String, min: 3, max: 1000},
     dish: {type: String, required: true, min: 3, max: 300},
     stars: {type: Number, required: true, min: 0, max: 5},
     review_text: {type: String, required: true, min: 3, max: 3000},
