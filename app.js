9ab7c780276cf9bf940af8d02b84c879c95e15bb
Guido Dizioli <guidodizi@gmail.com>
2018-03-29 11:59:39 -0300

    inital big future changes

--- app.js
+++ app.js
@@ -1,43 +1,50 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var review = require('./routes/review');
-
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var passport = require("passport");
+var flash = require("connect-flash");
+var session = require("express-session");
 
 var app = express();
 
 // Set up mongoose connection
-var mongoose = require('mongoose');
-var dev_db_url = 'mongodb://admin:admin@ds141464.mlab.com:41464/cookat_reviews'
+var mongoose = require("mongoose");
+var dev_db_url = "mongodb://admin:admin@ds141464.mlab.com:41464/cookat_reviews";
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'mariofavicon.ico')));
-app.use(logger('dev'));
+require("./config/passport")(passport); // pass passport for configuration
+
+app.use(favicon(path.join(__dirname, "public", "mariofavicon.ico")));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', review);
+// required for passport
+app.use(session({ secret: "cookatsecretsauce" })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+var routes = require("./routes/routes")(passport);
+
+app.use("/", routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -46,11 +53,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
